ce7597b628e196da5f9b3b93c3ba82e5b485b827
| Ali soleymani | <alisoleimani199@gmail.com>
2022-01-31 20:59:08 +0330

    Removed logs

--- src/utils/getFileList.js
+++ src/utils/getFileList.js
@@ -1,14 +1,12 @@
 export default function getFileList(event) {
-    let filedInfo = []
+  let filedInfo = []
 
-    console.log(event)
+  for (let i = 0; i < event.target.files.length; i++) {
+    filedInfo.push({
+      id: i,
+      name: event.target.files[i].name,
+    })
+  }
 
-    for (let i = 0; i < event.target.files.length; i++) {
-        filedInfo.push({
-            id: i,
-            name: event.target.files[i].name
-        })
-    }
-
-    return filedInfo
+  return filedInfo
 }
