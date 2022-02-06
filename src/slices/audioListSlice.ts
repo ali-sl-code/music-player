835894e5d0796553e497c8e3655787bb3d3d012b
| Ali soleymani | <alisoleimani199@gmail.com>
2022-02-06 21:20:34 +0330

    Handle audio list slice

---
+++ src/slices/audioListSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface AudioState {
+  playingAudioId: number | null,
+  audioList: [
+    {
+      id: number | null,
+      name: string | null
+    },
+  ]
+}
+
+export const initialState: AudioState = {
+  playingAudioId: 0,
+  audioList: [
+    {
+      id: null,
+      name: null,
+    },
+  ],
+}
+
+export const audioList = createSlice({
+  name: 'audioList',
+  initialState,
+  reducers: {
+    playingAudio: (
+      state,
+      action: PayloadAction<AudioState['playingAudioId']>,
+    ) => {
+      state.playingAudioId = action.payload
+    },
+    setAudioList: (state, action: PayloadAction<AudioState['audioList']>) => {
+      state.audioList = action.payload
+    },
+  },
+})
+
+export const { playingAudio, setAudioList } = audioList.actions
+
+export default audioList.reducer
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import audioDataSlice from './slices/audioDataSlice'
 import audioControlSlice from './slices/audioControlSlice'
+import audioListSlice from './slices/audioListSlice'
 
 export const store = configureStore({
   reducer: {
     audio: audioDataSlice,
     audioControl: audioControlSlice,
+    audioList: audioListSlice,
   },
 })
 
