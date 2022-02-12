7e3012a079e79e60f3780756fc699ef782e96242
| Ali soleymani | <alisoleimani199@gmail.com>
2022-02-12 17:28:19 +0330

    Add ErrorBoundary component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react'
+// import * as React from 'react'
 import ReactDOM from 'react-dom'
 // import 'module-alias/register'
-import { StyledEngineProvider } from '@mui/material/styles'
+// import { StyledEngineProvider } from '@mui/material/styles'
 import App from 'App'
-import { Auth0Provider } from '@auth0/auth0-react'
+// import { Auth0Provider } from '@auth0/auth0-react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { ErrorBoundary } from 'react-error-boundary'
+import Fallback from './components/ErrorFallback'
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 ReactDOM.render(
@@ -16,11 +18,14 @@ ReactDOM.render(
   //   redirectUri={window.location.origin}
   // >
   //  <StyledEngineProvider injectFirst></StyledEngineProvider>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ErrorBoundary FallbackComponent={Fallback}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ErrorBoundary>,
+
   // </Auth0Provider>
   document.querySelector('#root'),
 )
